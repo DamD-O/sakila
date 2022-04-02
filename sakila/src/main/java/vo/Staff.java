be11e9e5d569f202aeec8c7228c3a7a55c3118e9
project21-A6 <yedam212@gmail.com>
2022-04-02 10:51:19 +0900

    김예담 -> Staff.java 생성

---
+++ sakila/src/main/java/vo/Staff.java
@@ -0,0 +1,16 @@
+package vo;
+
+public class Staff {
+	private int staffId;
+	private String firstName;
+	private String lastName;
+	private int addressId;
+	public String picture;
+	public String email;
+	public int storeId;
+	public int active;
+	public String username;
+	public String password;
+	public String lastUpdate;
+	
+}
