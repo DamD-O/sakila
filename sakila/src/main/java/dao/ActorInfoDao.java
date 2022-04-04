b9b170394a5fe96203df6503d97cc4e9f2d8ae0f
DamD-O <yedam5063@gmail.com>
2022-04-04 13:01:02 +0900

    김예담 : actorinfo dao,list생성

---
+++ sakila/src/main/java/dao/ActorInfoDao.java
@@ -0,0 +1,47 @@
+package dao;
+
+import java.sql.*;
+import java.util.*;
+import util.DBUtil;
+import vo.ActorInfo;
+
+//페이징
+public class ActorInfoDao {
+	public List<ActorInfo> selectActorInfoListByPage(int beginRow, int rowPerPage){
+		List<ActorInfo> list = new ArrayList<ActorInfo>();
+		Connection conn = null;
+		//DBUtil dbUtil = new DBUtil(); //따로 생성한 db가져오기 -> static으로 객체생성 안해도 됨.
+		conn = DBUtil.getConnection(); //직접적으로 가져오지 않고 호출해서 가져온다.
+		
+		String sql = "SELECT actor_id actorId, first_name firstName, last_name lastName, film_info filmInfo FROM actor_info ORDER BY actor_id LIMIT ?,?";
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		try {
+			stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, beginRow);
+			stmt.setInt(2, rowPerPage);
+			rs = stmt.executeQuery();
+			//list.add
+			while(rs.next()) {
+				ActorInfo a = new ActorInfo();
+				a.setActorId(rs.getInt("actorId"));
+				a.setFirstName(rs.getString("firstName"));
+				a.setLastName(rs.getString("lastName"));
+				a.setFilmInfo(rs.getString("filmInfo"));
+				list.add(a);
+			}
+			
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				rs.close();
+				stmt.close();
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return list;
+	}
+}
