10b0d75471bed3a549d49343b246e8d71bf46b00
DamD-O <yedam5063@gmail.com>
2022-04-04 15:54:35 +0900

    김예담 : CustomerList 필드, toString ,getter,setter생성

--- sakila/src/main/java/vo/CustomerList.java
+++ sakila/src/main/java/vo/CustomerList.java
@@ -1,5 +1,78 @@
 package vo;
 
 public class CustomerList {
-
+	private int customerId;
+	private String name;
+	private String address;
+	private String postalCode;
+	private String phone;
+	private String city;
+	private String country;
+	private int active;
+	private int storeId;
+	
+	
+	@Override
+	public String toString() {
+		return "CustomerList [customerId=" + customerId + ", name=" + name + ", address=" + address + ", postalCode="
+				+ postalCode + ", phone=" + phone + ", city=" + city + ", country=" + country + ", active=" + active
+				+ ", storeId=" + storeId + "]";
+	}
+	
+	public int getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPostalCode() {
+		return postalCode;
+	}
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public int getActive() {
+		return active;
+	}
+	public void setActive(int active) {
+		this.active = active;
+	}
+	public int getStoreId() {
+		return storeId;
+	}
+	public void setStoreId(int storeId) {
+		this.storeId = storeId;
+	}
+	
+	
 }
