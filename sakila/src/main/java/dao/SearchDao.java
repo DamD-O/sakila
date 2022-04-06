2a506b885b136a52205a6fbf0a99de6ce4cce578
DamD-O <yedam5063@gmail.com>
2022-04-06 11:54:13 +0900

    김예담 : SearchDAO price,filmList 메소드 추가

--- sakila/src/main/java/dao/SearchDao.java
+++ sakila/src/main/java/dao/SearchDao.java
@@ -6,8 +6,37 @@ import java.util.List;
 
 import util.DBUtil;
 import vo.Category;
+import vo.FilmList;
 
-public class CategoryDao {
+public class SearchDao {
+	//영화 가격
+	public List<Double> selectfilmPriceList(){
+		List<Double> list = new ArrayList<Double>();
+		Connection conn = null; //연결변수
+		PreparedStatement stmt = null; //call변수
+		ResultSet rs = null; //결과저장 변수
+		conn = DBUtil.getConnection(); //db연결호출
+		String sql="SELECT DISTINCT price FROM film_list ORDER BY price";
+		try {
+			stmt =conn.prepareStatement(sql);
+			rs =stmt.executeQuery();
+			while(rs.next()) {
+				  list.add(rs.getDouble("price"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+	         try {
+	             rs.close();
+	             stmt.close();
+	             conn.close();
+	          } catch (SQLException e) {
+	             e.printStackTrace();
+	          }
+	       }
+	       return list;	
+	}
+	//영화 카테고리
    public List<Category> selectCategoryList() {
       List<Category> list = new ArrayList<Category>();
       Connection conn = null;
@@ -37,5 +66,25 @@ public class CategoryDao {
          }
       }
       return list;
+   }
+   
+   //영화검색
+   public List<FilmList>  selectFilmSerach(String category, String rating, Double price, int length, String title, String actor ){
+	   List<FilmList> list = new ArrayList<FilmList>();
+	   String sql ="";
+	   		if(category.equals("")&& rating.equals("") && price == -1 && length == -1) {
+	   			sql = "SELECT * from film_list WHERE title LIKE ? AND actor LIKE ?";
+	   		}else if(category.equals("") && rating.equals("")&& price == -1 && length!= -1) { //length만 입력됨.
+	   			if(length ==0) {
+	   				sql = "SELECT * from film_list WHERE title LIKE ? AND actors LIKE ? AND length < 60";
+	   			}else if(length == 1){
+	   				sql = "SELECT * from film_list WHERE title LIKE ? AND actors LIKE ? AND length >=60";
+	   			}
+	   		}else if(category.equals("")&& rating.equals("") && price != -1 && length == -1) {
+	   			sql = "SELECT * from film_list WHERE title LIKE ? AND actors LIKE ? AND price = ? ";
+	   		}
+	   
+	   return list;
+	   
    }
 }
